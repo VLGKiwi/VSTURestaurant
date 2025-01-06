8e3c454d7168d2c5e8cf930bdb67317982e5a3a0
Artem <60689988+VLGKiwi@users.noreply.github.com>
2025-01-07 00:48:16 +0300

    feat: delete back folder

--- backend/extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_mail import Mail
-from flask_cors import CORS
-
-db = SQLAlchemy()
-migrate = Migrate()
-mail = Mail()
-cors = CORS()
